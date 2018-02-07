9eb5b04126b2bf755f23b05ca412db3257bcd903
Jonas Greitemann <j.greitemann@lmu.de>
2018-02-07 17:12:52 +0100

    Deployment Wed Feb  7 17:12:52 CET 2018

--- svm-demo/ui.js
+++ svm-demo/ui.js
@@ -1,3 +1,4 @@
+var eps = 0.001;
 var add_point;
 var clear_points;
 var get_model;
@@ -8,8 +9,28 @@ var Module = {
     postRun: function () {
         add_point = Module.cwrap('add_point', null, ['number', 'number', 'number']);
         clear_points = Module.cwrap('clear_points', null, []);
-        get_model = Module.cwrap('get_model', 'number', ['number']);
+        get_model = {
+            'linear': Module.cwrap('get_model_linear', 'number', ['number', 'number', 'number']),
+            'quadratic': Module.cwrap('get_model_quadratic', 'number', ['number', 'number', 'number']),
+            'rbf': Module.cwrap('get_model_rbf', 'number', ['number', 'number', 'number']),
+            'sigmoid': Module.cwrap('get_model_sigmoid', 'number', ['number', 'number', 'number'])
+        };
         get_SV_coord = Module.cwrap('get_SV_coord', 'number', ['number']);
+
+        get_n_lines_zero = Module.cwrap('get_n_lines_zero', 'number', []);
+        get_n_line_points_zero = Module.cwrap('get_n_line_points_zero', 'number', ['number']);
+        get_line_points_zero = Module.cwrap('get_line_points_zero', 'number', ['number']);
+        get_line_closed_zero = Module.cwrap('get_line_closed_zero', 'number', ['number']);
+
+        get_n_lines_plus = Module.cwrap('get_n_lines_plus', 'number', []);
+        get_n_line_points_plus = Module.cwrap('get_n_line_points_plus', 'number', ['number']);
+        get_line_points_plus = Module.cwrap('get_line_points_plus', 'number', ['number']);
+        get_line_closed_plus = Module.cwrap('get_line_closed_plus', 'number', ['number']);
+
+        get_n_lines_minus = Module.cwrap('get_n_lines_minus', 'number', []);
+        get_n_line_points_minus = Module.cwrap('get_n_line_points_minus', 'number', ['number']);
+        get_line_points_minus = Module.cwrap('get_line_points_minus', 'number', ['number']);
+        get_line_closed_minus = Module.cwrap('get_line_closed_minus', 'number', ['number']);
     },
     print: function (text) {
         console.log(text);
@@ -25,6 +46,9 @@ window.onload = function () {
     var nu_slider = document.getElementById('nu-slider');
     var nu = parseFloat(nu_text.value);
     var SVcheckbox = document.getElementById('SVcheckbox');
+    var kernel_picker = document.getElementById('kernel-picker');
+    var gamma_text = document.getElementById('gamma-text');
+    var c0_text = document.getElementById('c0-text');
 
     var warning = document.getElementById('two-class-warning');
     var exception_box = document.getElementById('exception');
@@ -46,49 +70,223 @@ window.onload = function () {
     update_slider();
 
     var canvas = document.getElementById('canvas');
+    var boundingRect = {
+        origin: { x: -3.5, y: -2. },
+        size: { width: 7., height: 4. }
+    };
+    var sx = canvas.width / boundingRect.size.width;
+    var sy = canvas.height / boundingRect.size.height;
+    var mx = canvas.width / 2.;
+    var my = canvas.height / 2.;
+
     var ctx = canvas.getContext('2d');
 
     function getMousePos(evt) {
         var rect = canvas.getBoundingClientRect();
         return {
-            x: evt.clientX - rect.left,
-            y: evt.clientY - rect.top
+            x: (evt.clientX - rect.left - mx) / sx,
+            y: (evt.clientY - rect.top - my) / sy
         };
     }
 
-    function redraw() {
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
+    function edge_index(x, y) {
+        if (Math.abs(x - boundingRect.origin.x) < eps)
+            return 3;
+        if (Math.abs(y - boundingRect.origin.y) < eps)
+            return 0;
+        if (Math.abs(x - boundingRect.origin.x - boundingRect.size.width) < eps)
+            return 1;
+        if (Math.abs(y - boundingRect.origin.y - boundingRect.size.height) < eps)
+            return 2;
+        return -1;
+    }
 
-        var r = 5;
+    function traverse_boundary(ax, ay, bx, by, dir) {
+        var start_edge = edge_index(ax, ay);
+        var target_edge = edge_index(bx, by);
+        var current_edge = start_edge;
+        var x = ax, y = ay;
+        while (current_edge != target_edge) {
+            if (dir == 'ccw') {
+                switch (current_edge) {
+                case 0:
+                    x = boundingRect.origin.x;
+                    break;
+                case 1:
+                    y = boundingRect.origin.y;
+                    break;
+                case 2:
+                    x = boundingRect.origin.x + boundingRect.size.width;
+                    break;
+                case 3:
+                    y = boundingRect.origin.y + boundingRect.size.height;
+                    break;
+                }
+                ctx.lineTo(x, y);
+                current_edge = (current_edge + 3) % 4;
+            } else {
+                switch (current_edge) {
+                case 0:
+                    x = boundingRect.origin.x + boundingRect.size.width;
+                    break;
+                case 1:
+                    y = boundingRect.origin.y + boundingRect.size.height;
+                    break;
+                case 2:
+                    x = boundingRect.origin.x;
+                    break;
+                case 3:
+                    y = boundingRect.origin.y;
+                    break;
+                }
+                ctx.lineTo(x, y);
+                current_edge = (current_edge + 1) % 4;
+            }
+        }
+        ctx.lineTo(bx, by);
+    }
 
-        if (nice.length > 0 && naughty.length > 0) {
+    function update_active_parameters() {
+        gamma_text.disabled = kernel_picker.value === 'linear';
+        c0_text.disabled = (kernel_picker.value === 'linear'
+                            || kernel_picker.value === 'rbf');
+    }
+
+    function redraw(bare) {
+        ctx.setTransform(sx, 0, 0, sy, mx, my);
+        ctx.clearRect(boundingRect.origin.x, boundingRect.origin.y,
+                      boundingRect.size.width, boundingRect.size.height);
+
+        var r = 0.03;
+
+        if (!bare && nice.length > 0 && naughty.length > 0) {
             warning.style = "display: none;";
-            var ptr = get_model(nu);
-            if (ptr != 0) {
+            var kernel_name = kernel_picker.value;
+            var gamma = parseFloat(gamma_text.value);
+            var c0 = parseFloat(c0_text.value);
+            if (!(kernel_name in get_model))
+                alert('Unknown kernel: ' + kernel_name);
+            var res = get_model[kernel_name](nu, gamma, c0);
+            if (res != -1) {
                 hide_exception();
-                var a = Module.getValue(ptr, 'double');
-                var b = Module.getValue(ptr+8, 'double');
-                var rho = Module.getValue(ptr+16, 'double');
-
-                console.log('a = ' + a + ', b = ' + b + ', rho = ' + rho);
 
                 ctx.strokeStyle = '#999999';
-                ctx.lineWidth = 1;
+                ctx.lineWidth = 0.005;
                 ctx.fillStyle = '#dddddd';
-                ctx.beginPath();
-                ctx.moveTo(0, (rho - 1) / b);
-                ctx.lineTo(canvas.width, (rho - 1 - a * canvas.width) / b);
-                ctx.lineTo(canvas.width, (rho + 1 - a * canvas.width) / b);
-                ctx.lineTo(0, (rho + 1) / b);
-                ctx.fill();
-                ctx.stroke();
+                var a, b, rho;
+                if (res == 1) {
+                    var ptr = Module.ccall('get_model_coeffs', 'number', [], []);
+                    a = Module.getValue(ptr, 'double');
+                    b = Module.getValue(ptr+8, 'double');
+                    rho = Module.getValue(ptr+16, 'double');
+                    console.log('a = ' + a + ', b = ' + b + ', rho = ' + rho);
+
+                    ctx.beginPath();
+                    ctx.moveTo(boundingRect.origin.x,
+                               (rho - 1 - a * boundingRect.origin.x) / b);
+                    ctx.lineTo(boundingRect.origin.x + boundingRect.size.width,
+                               (rho - 1 - a * (boundingRect.origin.x + boundingRect.size.width)) / b);
+                    ctx.lineTo(boundingRect.origin.x + boundingRect.size.width,
+                               (rho + 1 - a * (boundingRect.origin.x + boundingRect.size.width)) / b);
+                    ctx.lineTo(boundingRect.origin.x,
+                               (rho + 1 - a * boundingRect.origin.x) / b);
+                    ctx.fill();
+                    ctx.stroke();
+                } else {
+                    // get trail point
+                    var tx = 0, ty = 0;
+                    if (get_n_lines_zero() > 0) {
+                        var ptr = get_line_points_zero(0);
+                        tx = Module.getValue(ptr + 16, 'double');
+                        ty = Module.getValue(ptr + 24, 'double');
+                    }
+
+                    ctx.beginPath();
+                    {
+                        var n_lines = get_n_lines_minus();
+                        for (var i = 0; i < n_lines; ++i) {
+                            var n_pt = get_n_line_points_minus(i);
+                            var ptr = get_line_points_minus(i);
+                            ctx.moveTo(Module.getValue(ptr, 'double'),
+                                       Module.getValue(ptr + 8, 'double'));
+                            for (var j = 1; j < n_pt; ++j) {
+                                ctx.lineTo(Module.getValue(ptr + 16*j, 'double'),
+                                           Module.getValue(ptr + 16*j + 8, 'double'));
+                            }
+                            if (get_line_closed_minus(i)) {
+                                ctx.lineTo(Module.getValue(ptr, 'double'),
+                                           Module.getValue(ptr + 8, 'double'));
+                            } else {
+                                traverse_boundary(Module.getValue(ptr + 16*(n_pt-1), 'double'),
+                                                  Module.getValue(ptr + 16*(n_pt-1) + 8, 'double'),
+                                                  Module.getValue(ptr, 'double'),
+                                                  Module.getValue(ptr + 8, 'double'),
+                                                  'ccw');
+                            }
+                        }
+                    }
+                    {
+                        var n_lines = get_n_lines_plus();
+                        for (var i = 0; i < n_lines; ++i) {
+                            var n_pt = get_n_line_points_plus(i);
+                            var ptr = get_line_points_plus(i);
+                            ctx.moveTo(Module.getValue(ptr, 'double'),
+                                       Module.getValue(ptr + 8, 'double'));
+                            for (var j = 1; j < n_pt; ++j) {
+                                ctx.lineTo(Module.getValue(ptr + 16*j, 'double'),
+                                           Module.getValue(ptr + 16*j + 8, 'double'));
+                            }
+                            if (get_line_closed_plus(i)) {
+                                ctx.lineTo(Module.getValue(ptr, 'double'),
+                                           Module.getValue(ptr + 8, 'double'));
+                            } else {
+                                traverse_boundary(Module.getValue(ptr + 16*(n_pt-1), 'double'),
+                                                  Module.getValue(ptr + 16*(n_pt-1) + 8, 'double'),
+                                                  Module.getValue(ptr, 'double'),
+                                                  Module.getValue(ptr + 8, 'double'),
+                                                  'ccw');
+                            }
+                        }
+                    }
+
+                    // isPointInPath uses canvas coordinate space: undo transform
+                    var ttx = tx * sx + mx;
+                    var tty = ty * sy + my;
+                    if (!ctx.isPointInPath(ttx, tty, 'evenodd')) {
+                        ctx.rect(boundingRect.origin.x, boundingRect.origin.y,
+                                 boundingRect.size.width, boundingRect.size.height);
+                    }
+                    ctx.fill('evenodd');
+                    ctx.stroke();
+                }
 
                 ctx.strokeStyle = 'black';
-                ctx.lineWidth = 2;
-                ctx.beginPath();
-                ctx.moveTo(0, rho / b);
-                ctx.lineTo(canvas.width, (rho - a * canvas.width) / b);
-                ctx.stroke();
+                ctx.lineWidth = 0.01;
+                if (res == 1) {
+                    ctx.beginPath();
+                    ctx.moveTo(boundingRect.origin.x,
+                               (rho - a * boundingRect.origin.x) / b);
+                    ctx.lineTo(boundingRect.origin.x + boundingRect.size.width,
+                               (rho - a * (boundingRect.origin.x + boundingRect.size.width)) / b);
+                    ctx.stroke();
+                } else {
+                    var n_lines = get_n_lines_zero();
+                    for (var i = 0; i < n_lines; ++i) {
+                        var n_pt = get_n_line_points_zero(i);
+                        var ptr = get_line_points_zero(i);
+                        ctx.beginPath();
+                        ctx.moveTo(Module.getValue(ptr, 'double'),
+                                   Module.getValue(ptr + 8, 'double'));
+                        for (var j = 1; j < n_pt; ++j) {
+                            ctx.lineTo(Module.getValue(ptr + 16*j, 'double'),
+                                       Module.getValue(ptr + 16*j + 8, 'double'));
+                        }
+                        if (get_line_closed_zero(i)) {
+                            ctx.closePath();
+                        }
+                        ctx.stroke();
+                    }
+                }
             } else {
                 except_str = Module.ccall('get_err_string', 'string', [], []);
                 show_exception(except_str);
@@ -96,6 +294,37 @@ window.onload = function () {
         } else {
             warning.style = "display: visible;";
         }
+
+        // coordinate system
+        ctx.strokeStyle = 'darkgray';
+        ctx.lineWidth = 0.0025;
+        ctx.setLineDash([0.01, 0.02]);
+        ctx.beginPath();
+        for (var y = Math.ceil(boundingRect.origin.y);
+             y < boundingRect.origin.y + boundingRect.size.height; y += 1.) {
+            if (Math.abs(y) > eps) {
+                ctx.moveTo(boundingRect.origin.x, y);
+                ctx.lineTo(boundingRect.origin.x + boundingRect.size.width, y);
+            }
+        }
+        for (var x = Math.ceil(boundingRect.origin.x);
+             x < boundingRect.origin.x + boundingRect.size.width; x += 1.) {
+            if (Math.abs(x) > eps) {
+                ctx.moveTo(x, boundingRect.origin.y);
+                ctx.lineTo(x, boundingRect.origin.y + boundingRect.size.height);
+            }
+        }
+        ctx.stroke();
+        ctx.strokeStyle = '#555555';
+        ctx.lineWidth = 0.0025;
+        ctx.setLineDash([0.05, 0.05]);
+        ctx.beginPath();
+        ctx.moveTo(boundingRect.origin.x, 0);
+        ctx.lineTo(boundingRect.origin.x + boundingRect.size.width, 0);
+        ctx.moveTo(0, boundingRect.origin.y);
+        ctx.lineTo(0, boundingRect.origin.y + boundingRect.size.height);
+        ctx.stroke();
+        ctx.setLineDash([]);
 
         ctx.fillStyle = 'green';
         nice.forEach(function (pt) {
@@ -108,8 +337,9 @@ window.onload = function () {
             ctx.fillRect(pt.x - r, pt.y - r, 2*r, 2*r);
         });
 
-        if (SVcheckbox.checked) {
+        if (!bare && SVcheckbox.checked) {
             ctx.strokeStyle = 'blue';
+            ctx.lineWidth = 0.006;
             var SV_ptr;
             for (var i = 0; SV_ptr = get_SV_coord(i); ++i) {
                 var SV_x = Module.getValue(SV_ptr, 'double');
@@ -135,29 +365,67 @@ window.onload = function () {
 
         add_point(pos.x, pos.y, 1 ? event.button == 0 : -1);
 
-        redraw();
+        redraw(false);
     }, false);
 
     nu_text.onchange = function (event) {
         nu = parseFloat(nu_text.value);
-        update_slider();
-        redraw();
+        if (nu != NaN) {
+            nu_text.value = nu;
+            update_slider();
+            redraw(false);
+        } else {
+            alert('Invalid number');
+        }
     };
 
     nu_slider.oninput = function (event) {
         nu = 0.01 * nu_slider.value;
         nu_text.value = nu;
-        redraw();
+        redraw(false);
     };
 
     SVcheckbox.onclick = function (event) {
-        redraw();
-    }
+        redraw(false);
+    };
 
     document.getElementById('clear-button').onclick = function (event) {
         nice = [];
         naughty = [];
         clear_points();
-        redraw();
-    }
+        hide_exception();
+        redraw(false);
+    };
+
+    kernel_picker.onclick = function (event) {
+        update_active_parameters();
+        redraw(false);
+    };
+
+    gamma_text.onclick = function (event) { gamma_text.select(); };
+    gamma_text.onchange = function (event) {
+        gamma = parseFloat(gamma_text.value);
+        if (gamma != NaN) {
+            gamma_text.value = gamma;
+            redraw(false);
+        } else {
+            alert('Invalid number');
+        }
+        gamma_text.select();
+    };
+
+    c0_text.onclick = function (event) { c0_text.select(); };
+    c0_text.onchange = function (event) {
+        c0 = parseFloat(c0_text.value);
+        if (c0 != NaN) {
+            c0_text.value = c0;
+            redraw(false);
+        } else {
+            alert('Invalid number');
+        }
+        c0_text.select();
+    };
+
+    update_active_parameters();
+    redraw(true);
 };
